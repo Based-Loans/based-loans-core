698dc07b14681b91426c7ffae3e6f72903ac9647
ghoul-sol <ghoul.sol@pm.me>
2021-03-24 23:41:05 -0500

    remove comment

--- deploy/006_deploy_uniswap_oracle.ts
+++ deploy/006_deploy_uniswap_oracle.ts
@@ -12,7 +12,6 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const bETH = await deployments.get('CEther');
   const bUSDC = await deployments.get('CErc20Immutable.bUSDC');
   const bWBTC = await deployments.get('CErc20Immutable.bWBTC');
-  // const bESD = await deployments.get('CErc20Immutable.bESD');
 
   let bEthConfig = config.marketsConfig.bETH.tokenConfig;
   bEthConfig.cToken = bETH.address;
