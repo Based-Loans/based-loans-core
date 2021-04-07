d87c6e88e1a910199885fe1320e5194685315702
ghoul-sol <ghoul.sol@pm.me>
2021-04-07 09:19:47 -0500

    rinkeby config for token distribution

--- config/tokenDistribution.js
+++ config/tokenDistribution.js
@@ -8,7 +8,7 @@ const mainnetConfig = {
     rewardAmount: '35000000' + '000000000000000000', // 35 mil tokens
     startTime: 	1618617600, // From 2021-04-17:00:00:00 UTC
   },
-  Comp: {
+  comp: {
     stakingTokenAddress: '0xc00e94cb662c3520282e6f5717214004a7f26888',
     duration: 120, // 2 minutes
     rewardAmount: '12000000' + '000000000000000000', // 12 mil tokens
@@ -16,7 +16,20 @@ const mainnetConfig = {
   }
 }
 
-const rinkebyConfig = mainnetConfig;
+const rinkebyConfig = {
+  wbtc: {
+    stakingTokenAddress: '0xb480C498F33a664DD43Ffab82D9c49B073Db8b2c',
+    duration: 120, // 2 minutes
+    rewardAmount: '35000000' + '000000000000000000', // 35 mil tokens
+    startTime: 	1618617600, // From 2021-04-17:00:00:00 UTC
+  },
+  usdc: {
+    stakingTokenAddress: '0xeb8f08a975Ab53E34D8a0330E0D34de942C95926',
+    duration: 120, // 2 minutes
+    rewardAmount: '12000000' + '000000000000000000', // 12 mil tokens
+    startTime: 	1618617600, // From 2021-04-17:00:00:00 UTC
+  }
+};
 
 module.exports = {
   mainnet: mainnetConfig,
