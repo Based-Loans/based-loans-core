7d1efde189678bcb95ad7be43a9500dbb62a4749
ghoul-sol <ghoul.sol@pm.me>
2021-04-08 21:02:51 -0500

    deployment for token vesting contracts

---
+++ deploy/015_token_vesting.ts
@@ -0,0 +1,30 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import * as CONFIG from '../config';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy, execute, read } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const fourYears = 126144000;
+  const start = 1618617600;
+  const cliff = 0;
+  const revocable = false;
+
+  const vestings = [
+    // [deployer, start, cliff, fourYears, revocable],
+  ]
+
+  for (let index = 0; index < vestings.length; index++) {
+    let vest = vestings[index];
+    await deploy("TokenVesting" + index, {
+      contract: "TokenVesting",
+      from: deployer,
+      log: true,
+      args: vest
+    })
+  }
+};
+export default func;
+func.tags = ['vesting'];
