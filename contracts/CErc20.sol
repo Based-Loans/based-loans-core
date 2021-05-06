8010a9f2ff6aed8ee5d84227327bc3d6b16f3560
ghoul-sol <ghoul.sol@pm.me>
2021-05-06 10:20:27 -0500

    fix #11

--- contracts/CErc20.sol
+++ contracts/CErc20.sol
@@ -120,6 +120,16 @@ contract CErc20 is CToken, CErc20Interface {
         return _addReservesInternal(addAmount);
     }
 
+    /**
+     * @notice A public function to sweep accidental ERC-20 transfers to this contract. Tokens are sent to admin (timelock)
+     * @param token The address of the ERC-20 token to sweep
+     */
+    function sweepToken(EIP20NonStandardInterface token) external {
+    	require(address(token) != underlying, "CErc20::sweepToken: can not sweep underlying token");
+    	uint256 balance = token.balanceOf(address(this));
+    	token.transfer(admin, balance);
+    }
+
     /*** Safe Token ***/
 
     /**
@@ -179,7 +189,7 @@ contract CErc20 is CToken, CErc20Interface {
      */
     function doTransferOut(address payable to, uint amount) internal override {
         require(to != address(0), "cannot send to zero address");
-        
+
         EIP20NonStandardInterface token = EIP20NonStandardInterface(underlying);
         token.transfer(to, amount);
 
